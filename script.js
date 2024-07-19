65d0d8c810646e6477b090d6f8cf556ac7db18c4
C7V2 <148872819+C7V2@users.noreply.github.com>
2024-07-20 02:08:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+var timer = 60;
+var score = 0;
+var hitrn = 0;
+
+function increaseScore(){
+    score += 10;
+    document.querySelector("#scoreval").textContent = score;
+}
+
+function getNewHit(){
+    hitrn = Math.floor(Math.random() * 10);
+    document.querySelector("#hitval").textContent = hitrn;
+}
+
+function makeBubble(){
+    var clutter = "";
+    for (var i = 1; i <= 176; i++){
+        var rn = Math.floor(Math.random() * 10);
+        clutter += `<div class="bubble">${rn}</div>`;
+    }
+    document.querySelector("#pbtm").innerHTML = clutter;
+}
+
+function runTimer(){
+    var timerint = setInterval(function(){
+        if (timer > 0){
+            timer--;
+            document.querySelector("#timerval").textContent = timer;
+        } else {
+            clearInterval(timerint);
+            document.querySelector("#pbtm").innerHTML = `<div class="game-over">
+                <p>Game Over</p>
+                <p>Your Score: ${score}</p>
+            </div>`;
+        }
+    }, 1000);
+}
+
+document.querySelector("#pbtm").addEventListener("click", function(dets){
+    var clicknum = Number(dets.target.textContent);
+    if (hitrn === clicknum){
+        increaseScore();
+        getNewHit();
+        makeBubble();
+    }
+});
+
+runTimer();
+makeBubble();
+getNewHit();
